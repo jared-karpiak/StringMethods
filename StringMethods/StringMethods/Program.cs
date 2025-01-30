c1de6525cb2350d2ff0df08da4ffb8dc3b7e5499
jared-karpiak <jkarpiak@nait.ca>
2025-01-30 08:23:43 -0700

    Added comments
    Added comments about arrays and how strings are character arrays.

--- StringMethods/StringMethods/Program.cs
+++ StringMethods/StringMethods/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Taylor Swift";
+
+            // Strings are technically an "array" of char types.
+            // An array is a type of collection that can be "indexed"
+            // Here the name "Taylor Swift" is a collection of 12 char elements (the white space is included).
+
+            // Each item in an array is given an "index", basically an 'id' of sorts.
+            // In C# arrays are 0-indexed (the first item is 0, the second is 1, etc.)
+            // For the string "Taylor Swift", the 0th character is 'T', the 7th character is 'S', and so on.
+
+            // 'T' + 'a' + 'y' + 'l' + 'o' + 'r' + ' ' + 'S' + 'w' + 'i' + 'f' + 't'
+            //  0     1     2     3     4     5     6     7     8     9     10    11
+
+
+
+
         }
     }
 }
