85374e675f1a96825ee1ae308338aa45c6442f52
jared-karpiak <jkarpiak@nait.ca>
2025-01-30 09:06:46 -0700

    More string methods
    Added even more string method examples and comments

--- StringMethods/StringMethods/Program.cs
+++ StringMethods/StringMethods/Program.cs
@@ -34,12 +34,58 @@
             // IndexOf() is going to give the number of the first occurrence of a given character or string
             // It returns an integer, the index of string.
             Console.WriteLine($"The space in {name} is located at {name.IndexOf(' ')}.");
-            Console.WriteLine($"The S in {name} is located at index {name.IndexOf('S')}.");
 
             // We can pass in a full string as well, and it will give us the index of the first character
             string subString = "lor";
             Console.WriteLine($"The substring '{subString}' in {name} is located at index {name.IndexOf(subString)}.");
-            
+
+            // If IndexOf() cannot find the string or substring, it will return a -1.
+            Console.WriteLine($"The Z in {name} is located at index {name.IndexOf('Z')}.");
+
+            // Contains() will tell us if a character or substring exists in the string.
+            // It returns a boolean (true/false)
+
+            Console.WriteLine($"Does {name} contain 'T'? {name.Contains('T')}.");
+            Console.WriteLine($"Does {name} contain \"ggff\"? {name.Contains("ggff")}.");
+
+            // To select a specific character inside of a string, we can identify it using its index.
+            // To choose the character using its index, we provide the name of the string variable,
+            // followed immediately by square brackets [] that contain inside of them the index.
+            // For example:
+
+            Console.WriteLine($"The first character of {name} is {name[0]}.");
+
+            // In C#, arrays are 0-indexed, meaning that in order to select the first character, we use
+            // the number 0, to get the second character we use the number 1, etc.
+            // If we want to get the fifth character in the string:
+
+            Console.WriteLine($"The fifth character of {name} is {name[4]}.");
+
+            // To get the last character in a string, we can combine the .Length method with -1 to get us the last index.
+            // ALWAYS make sure to include the -1 here, if you just use .Length, you will get an
+            // "Index Out Of Range" error, which is very bad.
+
+            //                                                string     .Length   -1
+            //                                                    |         |       |
+            //                                                    v         v       v
+            Console.WriteLine($"The last character of {name} is {name[name.Length - 1]}");
+
+            // The above case would be the same as doing name[11], but with name.Length - 1, we can
+            // can avoid the Out of Range error in case the string changes elsewhere in our code.
+
+
+            // To get a "sub string" (a small string inside of the big string) we can use the Substring method.
+            // Substring() takes two integers.
+            // The first int is the starting index, which is INCLUSIVE.
+            // The second int is the ending index, which is EXCLUSIVE.
+
+            //                                                 index of first char      index of space
+            //                                                              |                |
+            //                                                              v                v
+            Console.WriteLine($"The first name of {name} is {name.Substring(0, name.IndexOf(' '))}");
+
+
+
         }
     }
 }
