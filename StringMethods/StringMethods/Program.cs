4010c2aa28863bd8b80513477646d6fab27ef68d
jared-karpiak <jkarpiak@nait.ca>
2025-01-30 08:38:42 -0700

    More string methods
    Added some more demonstrations of various string methods.

--- StringMethods/StringMethods/Program.cs
+++ StringMethods/StringMethods/Program.cs
@@ -17,9 +17,29 @@
             // 'T' + 'a' + 'y' + 'l' + 'o' + 'r' + ' ' + 'S' + 'w' + 'i' + 'f' + 't'
             //  0     1     2     3     4     5     6     7     8     9     10    11
 
+            // the Length method will return the number of characters in the string.
+            Console.WriteLine($"The length of {name} is {name.Length}.");
 
+            // The ToUpper() method will convert all characters in the string to uppercase
+            // However it will not permanently change the variable to be uppercase
+            // To permanently change it we would have to reassign the variable, like this:
+            // name = name.ToUpper();
+            Console.WriteLine($"{name} in upper case is {name.ToUpper()}.");
+            Console.WriteLine($"This is {name} after the ToUpper() call.");
 
+            // There is also a ToLower() method, which is used the same way as ToUpper(),
+            // but instead makes all characters lower case.
+            Console.WriteLine($"{name} in lower case is {name.ToLower()}.");
 
+            // IndexOf() is going to give the number of the first occurrence of a given character or string
+            // It returns an integer, the index of string.
+            Console.WriteLine($"The space in {name} is located at {name.IndexOf(' ')}.");
+            Console.WriteLine($"The S in {name} is located at index {name.IndexOf('S')}.");
+
+            // We can pass in a full string as well, and it will give us the index of the first character
+            string subString = "lor";
+            Console.WriteLine($"The substring '{subString}' in {name} is located at index {name.IndexOf(subString)}.");
+            
         }
     }
 }
